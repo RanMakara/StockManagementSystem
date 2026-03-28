a37e45c3a9ea32b32cab6b933280c2b6c3becb04
Ran Makara <makara150350@gmail.com>
2026-03-28 12:30:56 +0700

    first commit

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@ const Logo = () => {
   return (
     <div className="flex justify-center mb-4">
       <div className="w-16 h-16 rounded-full bg-gradient-to-r from-blue-500 to-indigo-500 flex items-center justify-center shadow-lg">
-        <img src="p2.png" className="w-10 h-10 object-contain drop-shadow-sm" alt="logo" />
+        <img src="./img/logo/p2.png" className="w-20 h-20 object-contain drop-shadow-sm" alt="logo" />
       </div>
     </div>
   );
